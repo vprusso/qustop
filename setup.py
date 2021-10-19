7fdc8b30fb897ed267831113ebcbe923f0c422f4
vprusso <vincentrusso1@gmail.com>
2021-10-18 21:51:14 -0400

    Updating rtd.yml

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=requirements,
     test_suite="tests",
 )
